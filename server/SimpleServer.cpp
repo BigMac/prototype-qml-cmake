dc0313400c1c7d41fa6deabc20cf8ff44271e9d2
BigMac <bigmac@o2.pl>
2012-05-22 15:15:35 +0200

    Simple Server stub, boost dependency

---
+++ server/SimpleServer.cpp
@@ -0,0 +1,5 @@
+#include "SimpleServer.h"
+
+SimpleServer::SimpleServer()
+{
+}
---
+++ server/SimpleServer.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLESERVER_H
+#define SIMPLESERVER_H
+
+class SimpleServer
+{
+public:
+    SimpleServer();
+};
+
+#endif // SIMPLESERVER_H
