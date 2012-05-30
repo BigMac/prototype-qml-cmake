66faebd3d682f70535d2960afba5182da02fec23
BigMac <bigmac@o2.pl>
2012-05-30 16:40:59 +0200

    Entry point shows qml

--- client/gui/GuiEntryPoint.cpp
+++ client/gui/GuiEntryPoint.cpp
@@ -1,6 +1,11 @@
 #include "GuiEntryPoint.h"
 #include "InventoryGUI.h"
 #include "core/InventoryCore.h"
+#include <QDeclarativeEngine>
+#include <QDeclarativeComponent>
+#include <QDeclarativeContext>
+#include <QDeclarativeView>
+#include <QApplication>
 
 GuiEntryPoint::GuiEntryPoint()
 {
@@ -11,5 +16,10 @@ int GuiEntryPoint::exec(int argc, char *argv[], std::shared_ptr<InventoryCore> c
     std::shared_ptr<InventoryGUI> gui = std::make_shared<InventoryGUI>();
     gui->attachCore(core);
 
-    return 0;
+    QApplication app(argc, argv);
+    QDeclarativeView view;
+    view.setSource(QUrl::fromLocalFile("gui/main.qml"));
+    view.setVisible(true);
+    view.connect(view.engine(), SIGNAL(quit()), SLOT(close()));
+    return app.exec();
 }
